22fb0140bf0d6bd9c3f7e2e043b12dccb0b302c9
Anup Kumar Panwar <1anuppanwar@gmail.com>
2019-04-09 16:22:00 +0530

    completed

--- content.js
+++ content.js
@@ -1,26 +1,92 @@
-suggest()
+suggest();
 
 function suggest() {
-    var $textObj = $('input:text')
+    var $suggestionSpan = $("<span>", { id: "suggestion" })
+        .css("padding-left", "5px")
+        .text("");
+    var sentence = "";
 
-    var $countSpan = $('<span>', { id: 'count' }).css('padding-left', '5px').text('')
+    $("input:text").focus(function () {
+        var $textObj = $(this);
+        sentence = "";
+        var previousSentence = "";
+        var suggestion = "";
+        var delimeters = [".", "?", "!"];
+        $("#suggestion").html(suggestion);
 
-    $textObj.on('input', function () {
-        var size = 0
-        var value = $textObj.val()
-        for (var i = 0; i < value.length; i++) {
-            size += unescape(encodeURIComponent(value[i])).length
-        }
-        var suggestion = '';
-        $.get(
-            "https://www.google.com/complete/search?q=" + value + "&cp=3&client=psy-ab",
-            function (data) {
-                if (data[1]) {
-                    suggestion = data[1][0][0];
-                    $('#count').text(suggestion)
+        $textObj.on("input", function () {
+            var value = $textObj.val();
+            var last = 0;
+            for (i in delimeters) {
+                var delimeter = delimeters[i];
+                var pos = value.lastIndexOf(delimeter);
+                if (pos > last) {
+                    last = pos;
                 }
             }
-        );
-        $textObj.after($countSpan)
+
+            if (last + 1 >= value.length) {
+                sentence = "";
+                $("#suggestion").html("");
+            } else {
+                if (last == 0) {
+                    sentence = value.substring(last, value.length);
+                    previousSentence = value.substring(0, last);
+                } else {
+                    sentence = value.substring(last + 1, value.length);
+                    previousSentence = value.substring(0, last + 1);
+                }
+            }
+
+            $.get(
+                "https://www.google.com/complete/search?q=" +
+                sentence +
+                "&cp=3&client=psy-ab",
+                function (data) {
+                    if (data[1]) {
+                        suggestion = data[1][0][0];
+                        $("#suggestion").html(suggestion);
+                    }
+                }
+            );
+        });
+
+        $textObj.on("unfocus", function () {
+            suggestion = "";
+            $("#suggestion").html(suggestion);
+        });
+
+        $textObj.on("keydown", function (e) {
+            var keyCode = e.keyCode || e.which;
+            if (keyCode == 9) {
+                if ($("#suggestion").html() != "") {
+                    e.preventDefault();
+
+                    var formattedText = unescape(suggestion);
+                    formattedText = formattedText.replace(/<b>/g, "");
+                    formattedText = formattedText.replace(/<\/b>/g, "");
+                    formattedText = unescape(formattedText);
+
+                    var div = document.createElement("div");
+                    div.innerHTML = formattedText;
+                    formattedText = div.innerText || div.textContent || "";
+
+                    formattedText =
+                        formattedText[0].toUpperCase() +
+                        formattedText.substring(1, formattedText.length);
+                    var completeText = previousSentence + " " + formattedText;
+                    completeText = completeText.trim();
+
+                    $textObj.val(completeText);
+
+                    $("#suggestion").html("");
+                }
+            } else if (keyCode == 27) {
+                suggestion = "";
+                $("#suggestion").html("");
+            }
+        });
+
+        $textObj.after($suggestionSpan);
     });
 }
